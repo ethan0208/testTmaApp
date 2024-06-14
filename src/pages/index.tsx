68f8f416e1bcdae5b46493ce836af1db756883db
ethan0208 <ethan@anotherworld.game>
2024-06-14 16:46:55 +0900

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,6 @@
-import axios from 'axios';
 import { useEffect } from 'react';
 
 const MainPage = () => {
-  const BOT_TOKEN = '7100467674:AAGovrdVdIQ9b5UzpKgqJUYSYPNN8ZD9i20';
-  const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
-
-  const setWebhook = async () => {
-    try {
-      const url = 'http://localhost:4000';
-      await axios.post(`${TELEGRAM_API}/setWebhook`, { url });
-      console.log('Webhook set successfully');
-    } catch (error) {
-      console.error('Error setting webhook:', error);
-    }
-  };
-
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
       // Initialize Telegram Web Apps SDK
@@ -22,7 +8,6 @@ const MainPage = () => {
       // Customize the appearance of the Web App
       window.Telegram.WebApp.setHeaderColor('#f5f5f5');
       window.Telegram.WebApp.setBackgroundColor('#ffffff');
-      setWebhook();
     } else {
       console.error('Telegram Web Apps SDK not found');
     }
